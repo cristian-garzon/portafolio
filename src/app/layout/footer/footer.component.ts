955355b8cbbb01f56cfeed6ef14f7dfc85449b14
Cristian Camilo Garzon Rodriguez <cristiancamilogarzon2@gmail.com>
2021-09-01 22:49:59 -0500

    adding footer and social media

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
@@ -11,6 +10,10 @@ export class FooterComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  social(event: any): void{
+    window.open(event.target.attributes.id.value, "_blank");
   }
 
 }
